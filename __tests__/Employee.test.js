ae58fdabdf9fe641eefe6dedc99fbae81b9320fd
jrdrenth <john.drenth@gmail.com>
2021-03-06 18:59:07 -0800

    Implemented Employee class and subclasses, created unit tests for Employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,46 @@
+const Employee = require('../lib/Employee');
+
+describe('Employee', () => {
+
+  const testId = 1;
+  const testName = 'John';
+  const testEmail = 'email@gmail.com'
+
+  // Create new object to test with
+  const employee = new Employee(testId, testName, testEmail);
+
+  describe('Instantiation', () => {
+    it('should instantiate Employee instance and set properties with arguments passed in', () => {
+      expect(typeof(employee)).toBe('object');
+
+      // Verify that the new object has the correct property values assigned
+      expect(employee).toEqual({ id: testId, name: testName, email: testEmail });
+    });
+  });
+
+  describe('getId()', () => {
+    it('should get the Id of the Employee', () => {
+      expect(employee.getId()).toEqual(testId);
+    });
+  });
+
+  describe('getName()', () => {
+    it('should get the name of the Employee', () => {
+      expect(employee.getName()).toEqual(testName);
+    });
+  });
+
+  describe('getEmail()', () => {
+    it('should get the email of the Employee', () => {
+      expect(employee.getEmail()).toEqual(testEmail);
+    });
+  });
+
+  describe('getRole()', () => {
+    it('should get the role of the Employee', () => {
+      const expectedRole = 'Employee';
+      expect(employee.getRole()).toEqual(expectedRole);
+    });
+  });
+
+});
